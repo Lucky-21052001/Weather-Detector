e6b86c3e45414d58bf9719c53c76e4f1194fb0aa
Lucky <luckymishra310118@gmail.com>
2023-07-21 20:05:15 +0530

    initial commit

---
+++ weatherApp-Reactjs-master/weatherApp-Reactjs-master/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "bd2944214cbb9d294e024605a73c4468",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
